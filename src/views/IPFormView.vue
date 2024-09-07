259b981fc5a3ba2afa8741a9aab44121beb32f47
cherites <cherites0610@gmail.com>
2024-09-08 02:56:01 +0800

    feat:新增預約界面

--- src/views/IPFormView.vue
+++ src/views/IPFormView.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
-        {{ props.id }}
-        {{ clinic }}
+        {{ clinic?.name }}
+        {{ clinic?.phone }}
+    </div>
+    <div>
+        <input type="date" id="start" name="trip-start" value="2018-07-22" min="2018-01-01" max="2018-12-31" />
+        <input type="time" id="appt" name="appt" min="09:00" max="18:00" step="30" required />
     </div>
 </template>
 
@@ -14,18 +18,16 @@ import Clinic from '@/modal/clinic';
 const props = defineProps(['id'])
 const clinic = ref<Clinic>();
 
-
 const getAllInf = async () => {
     const req = await requests.get('http://192.168.88.180:5000/getAllIPInf')
-    const temp = req.data.result.results.filter((item) => item._id === props.id);
-
+    const temp = req.data.result.results.filter((item) => item._id == props.id);
     clinic.value = new Clinic(temp[0]._id, temp[0].address, temp[0].name, temp[0].phone)
 }
 
-onMounted(async () => {
-    console.log();
-    
-}),
+onMounted(() => {
+    getAllInf()
+
+})
 </script>
 
 <style scoped></style>
