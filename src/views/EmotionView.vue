6cb1594dad82ebecd1e3bac832c7fac228fe6376
cherites <cherites0610@gmail.com>
2024-09-07 17:59:10 +0800

    feat:情緒版初稿完成

--- src/views/EmotionView.vue
+++ src/views/EmotionView.vue
@@ -1,16 +1,29 @@
 <template>
     <div class="flex flex-col content-center flex-wrap mt-4 gap-4">
-
         <div>
             <div class="header flex flex-row justify-between">
-
                 <span class="font-semibold text-lg flex">
                     <PhPerson :size="32" color="#2d2020" />情緒共享
                 </span>
-                <span class="text-lg">更多></span>
             </div>
 
-            <div class="card flex flex-col h-64 w-80 rounded-lg shadow-xl p-5 gap-2">
+            <div class="card flex flex-col h-64 w-80 rounded-lg shadow-xl p-5 gap-2 overflow-hidden">
+                <div v-for="item in emotions" class="flex gap-3">
+                    <span>
+                        <PhHeartBreak :size="32" :color='color[item.category]' />
+                    </span>
+                    <span :style="{ color: color[item.category] }">{{ item.title }}</span>
+                </div>
+            </div>
+        </div>
+
+        <div>
+            <div class="header flex flex-row justify-between">
+                <span class="font-semibold text-lg flex">
+                    <PhUserCircle :size="32" color="#2d2020" />情緒獨享
+                </span>
+            </div>
+            <div class="card flex flex-col max-h-64 w-80 rounded-lg shadow-xl p-5 gap-2 overflow-hidden">
                 <div v-for="item in emotions" class="flex gap-3">
                     <span>
                         <PhHeartBreak :size="32" :color='color[item.category]' />
@@ -34,6 +47,10 @@ emotions.push(new Emotion(1, "加薪好開心", 0, "2024/09/07"))
 emotions.push(new Emotion(1, "jerry買晚餐不幫我帶好生氣", 1, "2024/09/07"))
 emotions.push(new Emotion(1, "忘記帶充電線哭哭", 2, "2024/09/07"))
 emotions.push(new Emotion(1, "思考晚餐吃甚麼好平靜", 3, "2024/09/07"))
+emotions.push(new Emotion(1, "加薪好開心", 0, "2024/09/07"))
+emotions.push(new Emotion(1, "加薪好開心", 0, "2024/09/07"))
+emotions.push(new Emotion(1, "加薪好開心", 0, "2024/09/07"))
+emotions.push(new Emotion(1, "加薪好開心", 0, "2024/09/07"))
 </script>
 
 <style scoped></style>
