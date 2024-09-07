61086888511641fd39c4be3a15563b5eeb7469ec
feather1217 <609sophie@gmail.com>
2024-09-08 03:00:27 +0800

    filx:修復bug

--- src/views/IPFormView.vue
+++ src/views/IPFormView.vue
@@ -18,18 +18,16 @@ import Clinic from '@/modal/clinic';
 const props = defineProps(['id'])
 const clinic = ref<Clinic>();
 
-
 const getAllInf = async () => {
     const req = await requests.get('http://192.168.88.180:5000/getAllIPInf')
-    const temp = req.data.result.results.filter((item) => item._id === props.id);
-
+    const temp = req.data.result.results.filter((item) => item._id == props.id);
     clinic.value = new Clinic(temp[0]._id, temp[0].address, temp[0].name, temp[0].phone)
 }
 
-onMounted(async () => {
-    console.log();
-    
-}),
+onMounted(() => {
+    getAllInf()
+
+})
 </script>
 
 <style scoped></style>
