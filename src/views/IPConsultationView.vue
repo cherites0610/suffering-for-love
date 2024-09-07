940c1f51294d6e2c942b56a2eee22aba4698cf38
feather1217 <609sophie@gmail.com>
2024-09-08 00:48:12 +0800

    feat: IP列表欄位

--- src/views/IPConsultationView.vue
+++ src/views/IPConsultationView.vue
@@ -9,11 +9,27 @@
                 <PhMagnifyingGlass :size="32" color="#e8e3e3" />
             </div>
         </div>
+        <br/>
+        <div>
+            <div class="flex flex-row mx-2">
+                <PhCircle :size="12" color="#5ab4c5" weight="fill"  class=" my-1"/>
+                <div class="grid grid-rows-4">
+                    <span>羽毛心靈雞湯診所</span>
+                    <span>大同路297號1樓</span>
+                </div>
+                
+                <PhGreaterThan :size="32" color="#000000" class="ml-auto"/>
+            </div>
+            
+
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import request from "../axios/index"
+
+
 
 request.get('https://data.taipei/api/v1/dataset/9267a378-744e-4a46-ac6c-aacc277402b4?scope=resourceAquire');
 </script>
