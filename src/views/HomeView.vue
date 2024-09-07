dc7aefd7bef8e3b659f58b2bdda6e7b8080909f5
feather1217 <609sophie@gmail.com>
2024-09-07 19:37:29 +0800

    feat:主介面修改完成

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,36 +1,39 @@
+import 
+
 <template>
     <div class="flex flex-col content-center flex-wrap mt-4 gap-4">
         <div>
             <div class="header flex flex-row justify-between">
-                <span class="font-semibold text-lg">誰可以做諮商?</span>
-                <span class="text-lg">更多></span>
+                <span class="font-semibold text-lg ">誰可以做諮商?</span>
+                <span class="text-lg">更多 ></span>
             </div>
-            <div class="card flex flex-row h-52 w-80 rounded-lg shadow-xl">
-                <div class="img h-100 flex-4 rounded-l-lg bg-black basis-1/3">
-                    123
+            <div class="card flex flex-row h-48 w-80 rounded-lg shadow-md overflow-hidden my-1">
+                <div class="bg-contain h-100 flex-4 rounded-l-lg bg-black basis-1/3">
+                    <img src="C:\Users\User\Desktop\suffering-for-love\image\347620.jpg"
+                    class=" ">
                 </div>
-                <span class="text-base basis-2/3">文字簡述</span>
+                <span class="text-base basis-2/3 ps-3 pe-3 py-5 leading-6">文字簡述文字簡述文字簡述文字簡述文字簡述文字簡述文字簡述文字簡述文字簡述文字簡述文字簡述文字簡述文字簡述文字簡述文字簡述文字簡述文字簡述文字簡述</span>
             </div>
         </div>
 
         <div>
             <div class=" font-semibold text-lg">服務列表</div>
-            <div class="card flex flex-row h-24 w-80 rounded-lg shadow-xl">
+            <div class="card flex flex-row h-24 w-80 rounded-lg shadow-md my-1">
                 <div class="basis-1/4 flex justify-center items-center flex-col" @click="goToUrl('/IPConsultation')">
                     <PhNotePencil color="#468D9B" :size="32" />
-                    <span class="text-base">實體看診</span>
+                    <span class=" text-base py-1">實體看診</span>
                 </div>
                 <div class="basis-1/4 flex justify-center items-center flex-col" @click="goToUrl('/OLConsultation')">
                     <PhPhoneTransfer color="#468D9B" :size="32" />
-                    <span class="text-base">視訊看診</span>
+                    <span class="text-base py-1">視訊看診</span>
                 </div>
                 <div class="basis-1/4 flex justify-center items-center flex-col" @click="goToUrl('/emotion')">
                     <PhHeartBreak color="#468D9B" :size="32" />
-                    <span class="text-base">心情版</span>
+                    <span class="text-base py-1">心情版</span>
                 </div>
                 <div class="basis-1/4 flex justify-center items-center flex-col" @click="goToUrl('/QA')">
                     <PhQuestionMark color="#468D9B" :size="32" />
-                    <span class="text-base">Q&A</span>
+                    <span class="text-base py-1">Q&A</span>
                 </div>
             </div>
         </div>
@@ -38,10 +41,10 @@
         <div>
             <div class="header flex flex-row justify-between">
                 <span class="font-semibold text-lg">更多消息</span>
-                <span class="text-lg">更多></span>
+                <span class="text-lg">更多 ></span>
             </div>
 
-            <div class="card flex flex-col h-52 w-80 rounded-lg shadow-xl overflow-hidden p-5">
+            <div class="card flex flex-col h-52 w-80 rounded-lg shadow-md overflow-hidden p-5 my-1">
                 <div class="flex justify-between mb-5" v-for="item in news" @click="goToUrl(item.url)">
                     <span class=" text-base">{{ item.title }}</span>
                     <span class=" text-base">{{ item.date }}</span>
