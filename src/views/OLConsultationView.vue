06fe905c4d442ea40195d85abd6aab0141d16278
feather1217 <609sophie@gmail.com>
2024-09-08 04:38:48 +0800

    feat:視訊諮商主介面100%

--- src/views/OLConsultationView.vue
+++ src/views/OLConsultationView.vue
@@ -1,25 +1,44 @@
 <template>
     <div>
-        <div class="header flex flex-row gap-5 m-5">
-            <div>
-                <input type="date" id="start" name="trip-start" value="2018-07-22" min="2018-01-01" max="2018-12-31" />
-                <input type="time" id="appt" name="appt" min="09:00" max="18:00" step="30" required />
-            </div>
-            <div class=" bg-cyan-200 rounded-full p-1">
-                <PhMagnifyingGlass @click="getAllInf()" :size="32" color="#5AB4C5" />
-            </div>
+        <div class=" mx-5 my-3">
+            <span> 選擇時段 :</span>
         </div>
-        <br />
+        
+        <div class="header flex flex-row gap-5 mx-5 my-3">
+            <div>
+                <input class=" focus:border-color600 focus:outline-none" type="date" id="start" name="trip-start" value="2018-07-22" min="2018-01-01" max="2018-12-31" />
+                <div class="grid grid-cols-2">
+                    <input class="my-2  focus:border-color600 focus:outline-none" type="time" id="appt" name="appt" min="09:00" max="18:00" step="30" required />           
+                    <PhMagnifyingGlass class="mx-3 my-1" @click="getAllInf()" :size="28" color="#5ab4c5" weight="duotone" />           
+                </div>
+            </div>
+           
+        </div>
+        
         <div>
-            <div v-for="item of clinics" @click="clickClinics(item._id)" class="flex flex-row mx-2">
-                <PhCircle :size="12" color="#5ab4c5" weight="fill" class=" my-1" />
-                <div class="grid grid-rows-4">
+            <div style="height: 0.5px; background-color: #000; margin-top: 5px;"></div>
+            <br/>
+            <div v-for="item of clinics" @click="clickClinics(item._id)" class="flex flex-row mx-4">
+                <PhCircle :size="12" color="#5ab4c5" weight="fill" class=" my-2" />
+                <div class="grid grid-rows-4 mx-3">
                     <span>{{ item.name }}</span>
-                    <span>{{ item.phone }}</span>
+                    <span style="color: #ADB8BE;">{{ item.phone }}</span>
                 </div>
 
-                <PhGreaterThan :size="32" color="#000000" class="ml-auto" />
+                <PhGreaterThan :size="25" color="#000000" class="ml-auto my-2" />
             </div>
+
+
+
+
+
+
+
+
+
+
+
+
         </div>
     </div>
 </template>
@@ -32,7 +51,7 @@ import router from '@/router';
 const clinics = ref<Clinic[]>([]);
 
 const getAllInf = async () => {
-    const req = await requests.get('http://127.0.0.1:5000/getAllOLInf')
+    const req = await requests.get('http://192.168.88.180:5000/getAllOLInf')
     clinics.value = []
 
     for (const item of req.data.result.results) {
