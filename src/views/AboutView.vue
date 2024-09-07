0cdb465a86a296608ce19dc82b3155e863137dbc
cherites <cherites0610@gmail.com>
2024-09-08 02:13:19 +0800

    fix:修復界面跑板

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -10,7 +10,28 @@
         <span style="color:#356C77"> 誰可以被心理諮商?</span>
     </div>
     <div class=" m-5 fon text-base">心理諮商確實是一種多元的工具，既可以作為療癒情感的方式，也能促進個人成長與自我探索。</div>
-    <div class=" m-5">ddd</div>
+    <div class="flex flex-row m-7">
+        <span class="text-base" >1、</span>
+        <span class="text-base">情緒或心理問題影響日常生活者：當個人的情緒狀態開始干擾日常生活，比如出現睡眠困難、食慾不振、持續的低落情緒、感到壓力沉重或焦躁不安時。無論是已經明確知道問題來源，還是對問題的原因不確定，諮商都能提供支持。</span>
+    </div>
+    <div class="flex flex-row m-7">
+        <span class="text-base" >2、</span>
+        <span class="text-base">自我探索與增強心理韌性者：即使認為生活中沒有明顯困難，心理諮商也適合想要進行自我探索的人，可以發掘自己的生命熱情、追尋人生目標。此外，心理諮商也是提升情緒管理與抗壓性、增強心理韌性的重要途徑之一。</span>
+    </div>
+    <div class=" m-5 fon">
+        <span style="color:#356C77" class="text-lg">心理諮商後會有的效果 :</span>
+    </div>
+    <div class=" m-5 fon text-base">
+        有些人會期望心理師給予「正確答案」，但其實諮商師是協助我們看看不順利的點在哪裡，一直卡住、過不去的關卡在哪裡，針對卡關的部份去做討論，一起找出突破口，因為個案往往不是因為「該怎麼做?」而卡住，多半是因為情緒的影響。諮商師與個案一起合作整理情緒，個案就能在自然而然中找到自己的潛在答案。
+    </div>
+    <div class=" m-5 fon">
+        <span style="color:#356C77" class="text-lg">心理諮商與心理治療的差別 :</span>
+    </div>
+    <img src="../../image/about_02.jpg" />
+    <div class="flex flex-row m-7">
+        <span class="text-base" >圖片來源 :</span>
+        
+    </div>
 </template>
 
 <script setup>
